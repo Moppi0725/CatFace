49a6a4b4eddc0b18159d8ba1bc0f9ea71898125e
大塚智貴 <moppi@ootsukasatoshitakanoMacBook-Pro.local>
2023-07-11 20:12:21 +0900

    main_test.goの修正

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -2,13 +2,31 @@ package main
 
 import "testing"
 
-func Example_Main() { 
-	goMain([]string{})
-	// Output:
-  	// Hello World
+func Help() {
+	goMain([]string{"./cwf", "--help"})
+	// main cwf [オプション] <県名>
+
+	// オプションをつけずに実行した場合は，現在の天気を出力する
+
+	// オプション
+	// 		-w, --week    このオプションはその週の天気予報を出力します．
+	// 		-h, --help    このメッセージを出力して終了する
+	// 		-v, --version バージョンを出力して終了する
+	// 変数
+	// 		県名    調べたい都道府県を指定する．
 }
-func Test_Main(t *testing.T) {
-	if status := goMain([]string{}); status != 0 { 
-		t.Error("Expected 0, got ", status)
-	}
+
+func Test_noOption_noCity(t *testing.T) {
+	goMain([]string{"./cwf"})
+	// main cwf [オプション] <県名>
+
+	// オプションをつけずに実行した場合は，現在の天気を出力する
+
+	// オプション
+	// 		-w, --week    このオプションはその週の天気予報を出力します．
+	// 		-h, --help    このメッセージを出力して終了する
+	// 		-v, --version バージョンを出力して終了する
+	// 変数
+	// 		県名    調べたい都道府県を指定する．
+
 }
